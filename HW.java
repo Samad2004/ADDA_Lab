129d2b7e474cbe07309a75670c7891dc3f10a976
Samad2004 <152531686+Samad2004@users.noreply.github.com>
2024-04-15 14:15:44 +0530

    Update HW.java

--- HW.java
+++ HW.java
@@ -6,6 +6,6 @@ class HW
     public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
         String name= sc.nextLine();
-        System.out.println("My name is +" name);
+        System.out.println("My name is "+ name);
     }
 }
