9a7e555b3978febb46d62d2b2f8b66307a111b0c
Samad2004 <152531686+Samad2004@users.noreply.github.com>
2024-04-15 14:12:08 +0530

    Update HW.java

--- HW.java
+++ HW.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 class HW
 
 {
     public static void main(String [] args){
-        System.out.println("Hello World");
+        Scanner sc=new Scanner(System.in);
+        String name= sc.nextLine();
+        System.out.println("My name is +" name);
     }
 }
